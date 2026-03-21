e9e09cdf298a1525ebdf88ade667b088de93df7a
Thwng21 <thuong215204@gmail.com>
2026-03-22 01:10:43 +0700

    update: ui vehicle, profile

--- fix-viet2.js
+++
@@ -1,8 +0,0 @@
-﻿const fs = require('fs');
-let file = fs.readFileSync('src/app/users/profile/page.tsx', 'utf8');
-
-file = file.replace(/Du\?i 4 ch\?/g, 'Dưới 4 chỗ');
-file = file.replace(/T\? 4 d\?n 10 ch\?/g, 'Từ 4 đến 10 chỗ');
-file = file.replace(/L\?n hon 10 ch\?/g, 'Lớn hơn 10 chỗ');
-
-fs.writeFileSync('src/app/users/profile/page.tsx', file, 'utf8');
--- fix-viet5.js
+++
@@ -1,7 +0,0 @@
-﻿const fs = require('fs');
-let file = fs.readFileSync('src/app/users/profile/page.tsx', 'utf8');
-
-file = file.replace(/<inpuô tôype/g, '<input type');
-file = file.replace(/<inputtype/g, '<input type');
-
-fs.writeFileSync('src/app/users/profile/page.tsx', file, 'utf8');
